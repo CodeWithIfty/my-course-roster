2132754797b7d5d1bf1effe4ee6c0366c07d1c6e
CodeWithIftekhar <codewithiftekhar@gmail.com>
2023-09-14 19:37:12 +0600

    created 9 json data and added courses.json file on public folder also added header component and some other small  changes

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="container mx-auto text-center p-6">
+      <h1 className="text-3xl font-bold">Course Registration</h1>
+    </header>
+  )
+}
+
+export default Header
